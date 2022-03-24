d331a41cae1d5c9e0a4d9e159f43db2ec340b567
Daniel Lorenzo <juandaniellorenzocabrera@alumno.ieselrincon.es>
2022-03-24 16:19:18 +0000

    Refactor in user js

--- components/User.js
+++ components/User.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { Text, View, Image, StyleSheet ,Pressable} from "react-native";
+import React, { useState } from "react";
+import { Text, View, Image, StyleSheet, Pressable } from "react-native";
 
 const User = (props) => {
   const randomGenre = () => (Math.random() < 0.5 ? "female" : "male");
@@ -8,23 +8,21 @@ const User = (props) => {
   };
   const picture = `https://minimaltoolkit.com/images/randomdata/${randomGenre()}/${randomPic()}.jpg`;
   const [state, changeState] = useState(false);
-  const changeStyle = () => changeState(state => !state);
- 
-  return (
+  const changeStyle = () => changeState((state) => !state);
 
-    <Pressable onPress={()=> {
-            changeStyle()}}>
+  return (
+    <Pressable onPress={() => { changeStyle(); }}>
       <View style={state === false ? styles.user : styles.activeUser}>
-            <Image source={{ uri: picture }} style={styles.userPhoto} />
-            <View style={styles.fontContainer}>
-                <Text style={styles.fonts}>
-                <Text style={styles.textTag}>Name:</Text> {props.name}
-                </Text>
-                <Text style={styles.fonts}>
-                    <Text style={styles.textTag}>Age:</Text> {props.age}
-                </Text>
-            </View>
-    </View>
+        <Image source={{ uri: picture }} style={styles.userPhoto} />
+        <View style={styles.fontContainer}>
+          <Text style={styles.fonts}>
+            <Text style={styles.textTag}>Name:</Text> {props.name}
+          </Text>
+          <Text style={styles.fonts}>
+            <Text style={styles.textTag}>Age:</Text> {props.age}
+          </Text>
+        </View>
+      </View>
     </Pressable>
   );
 };
@@ -50,20 +48,20 @@ const styles = StyleSheet.create({
     fontSize: 24,
     margin: 5,
   },
-  textTag : {
-      fontWeight : 'bold'
+  textTag: {
+    fontWeight: "bold",
   },
-  activeUser : {
-      backgroundColor : '#9B97B2',
-      height : '100%',
-      flex : 1,
-      justifyContent : 'center',
-      flexDirection : 'column',
-      alignItems : 'center',
-      padding : 10,
-      marginVertical : 10,
-      borderRadius: 10,
-  }
+  activeUser: {
+    backgroundColor: "#9B97B2",
+    height: "100%",
+    flex: 1,
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 10,
+  },
 });
 
 export default User;
